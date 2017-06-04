e166c5121ae042bd02afc3d13625245e3aec52f8
ChieftainY2k <ChieftainY2k@gmail.com>
2017-06-04 16:11:04 +0200

    README update Script update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-* Get Raspberry PI, cpnfigure it so that it can connect to the internet.
+* Get Raspberry PI, cnfigure it so that it can connect to the internet.
 
 * Install docker
 <pre>
@@ -7,16 +7,6 @@ $ sudo usermod pi -aG docker
 $ reboot
 </pre>
         
-* Install python PIP
-<pre>
-$sudo apt-get install python3-pip
-</pre>
-
-* Install docker-compose
-<pre>
-$ sudo pip3 install docker-compose
-</pre>
-
 * Build docker image
 <pre>
 $ cd docker
@@ -25,21 +15,36 @@ $ docker build -t streamer .
  
 * Initiate and run streamer for the first time
 <pre>
-$  docker run --privileged -e STREAMER_YOUTUBE_SECRET="xxxxxxxxxxxxxxxx" -e STREAMER_BITRATE=500000 -e STREAMER_FPS=30 --name camera streamer
+$ docker run --privileged -e STREAM_YOUTUBE_SECRET="xxxxxxxxxxxxxxxx" -e STREAM_BITRATE=500000 -e STREAM_FPS=30 -e STREAM_WIDTH=800 -e STREAM_HEIGHT=600 --name camera streamer
 </pre>
 
 * Stop streamer (may take a while)
 <pre>
-$  docker stop camera  
+$ docker stop camera  
 </pre>
 
 * Start streamer again
 <pre>
-$  docker start camera
+$ docker start camera
 </pre>
 
 * Attach to streamer console
 <pre>
-$  docker attach camera
+$ docker attach camera
+</pre>
+
+* Kill streamer process and remove container
+<pre>
+$ docker rm -f camera
+</pre>
+
+* (optional) Install python PIP
+<pre>
+$sudo apt-get install python3-pip
+</pre>
+
+* (optional) Install docker-compose
+<pre>
+$ sudo pip3 install docker-compose
 </pre>
 
--- docker/stream-youtube.sh
+++ docker/stream-youtube.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-if [ -z "$STREAMER_YOUTUBE_SECRET" ]; then
-    echo STREAMER_YOUTUBE_SECRET is empty, exiting.
+if [ -z "$STREAM_YOUTUBE_SECRET" ]; then
+    echo STREAM_YOUTUBE_SECRET is empty, exiting.
     exit
 fi
 
-if [ -z "$STREAMER_BITRATE" ]; then
-    STREAMER_BITRATE=3000000
-    echo STREAMER_BITRATE is empty, falling back to default: $STREAMER_BITRATE
+if [ -z "$STREAM_BITRATE" ]; then
+    STREAM_BITRATE=3000000
+    echo STREAM_BITRATE is empty, falling back to default: $STREAM_BITRATE
 fi
 
-if [ -z "$STREAMER_FPS" ]; then
-    STREAMER_FPS=25
-    echo STREAMER_BITRATE is empty, falling back to default: $STREAMER_BITRATE
+if [ -z "$STREAM_FPS" ]; then
+    STREAM_FPS=25
+    echo STREAM_BITRATE is empty, falling back to default: $STREAM_BITRATE
 fi
 
-echo Youtube key = $STREAMER_YOUTUBE_SECRET
-echo Video bitrate = $STREAMER_BITRATE
-echo Video FPS = $STREAMER_FPS
+if [ -z "$STREAM_WIDTH" ]; then
+    STREAM_WIDTH=1920
+    echo STREAM_WIDTH is empty, falling back to default: $STREAM_WIDTH
+fi
 
-raspivid -o - -t 0 -w 1920 -h 1080 -fps $STREAMER_FPS -b $STREAMER_BITRATE -g 40 | ffmpeg \
+if [ -z "$STREAM_HEIGHT" ]; then
+    STREAM_HEIGHT=1080
+    echo STREAM_HEIGHT is empty, falling back to default: $STREAM_HEIGHT
+fi
+
+echo ----------------------------------------------------------------------
+echo Initiating stream with following params:
+echo Youtube key = $STREAM_YOUTUBE_SECRET
+echo Stream bitrate = $STREAM_BITRATE
+echo Stream FPS = $STREAM_FPS
+echo Stream width = $STREAM_WIDTH
+echo Stream height = $STREAM_HEIGHT
+echo ----------------------------------------------------------------------
+
+raspivid -o - -t 0 -w $STREAM_WIDTH -h $STREAM_HEIGHT -fps $STREAM_FPS -b $STREAM_BITRATE -g 40 | ffmpeg \
 	-re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
 	-f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental \
-	-f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$STREAMER_YOUTUBE_SECRET
-
-
+	-f flv -r $STREAM_FPS rtmp://a.rtmp.youtube.com/live2/$STREAM_YOUTUBE_SECRET
 
